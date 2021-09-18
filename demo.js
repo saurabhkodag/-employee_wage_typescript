2a054e48a087b79b452985b584181fc50dae9ae7
Sourabh Madhukar Kodag <kodag.saurabh6@gmail.com>
2021-09-19 04:47:05 +0530

    [sourabh]: added UC2_Daily_Wage

--- demo.js
+++ demo.js
@@ -6,3 +6,9 @@
 // else{
 //     console.log("absent");
 // }
+//UC2 daily wage
+// let fulltime:number=8;
+// let wageperhour:number=20;
+// let empwage:number=0;
+// empwage=fulltime*wageperhour;
+// console.log(empwage);
--- demo.ts
+++ demo.ts
@@ -1,3 +1,4 @@
+
 //UC1 attendance
 // let emp:number=Math.floor(Math.random()*10%2)
 
@@ -7,3 +8,11 @@
 // else{
 //     console.log("absent");
 // }
+
+//UC2 daily wage
+
+// let fulltime:number=8;
+// let wageperhour:number=20;
+// let empwage:number=0;
+// empwage=fulltime*wageperhour;
+// console.log(empwage);
