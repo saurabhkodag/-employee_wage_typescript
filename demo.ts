a5c6bc6a721736cca82d733dccb52b6ebbb4a4d5
Sourabh Madhukar Kodag <kodag.saurabh6@gmail.com>
2021-09-19 04:51:02 +0530

    [sourabh]: added UC4_Switchcase

--- demo.ts
+++ demo.ts
@@ -16,3 +16,25 @@
 // let empwage:number=0;
 // empwage=fulltime*wageperhour;
 // console.log(empwage);
+
+// console.log(empwage);
+
+//UC4 switch case
+// let fulltime:number=8;
+// let parttime:number=4;
+// let wageperhour:number=20;
+// let emphrs:number=0;
+// let empwage:number=0;
+// let key:number =Math.floor(Math.random()*10%3)
+// switch(key){
+//     case 0:
+//     emphrs=fulltime;
+//     break;
+//   case 1:
+//     emphrs=parttime;
+//     break;
+//   default:
+//     emphrs=0;
+// }
+// empwage+=emphrs*wageperhour;
+// console.log(empwage);
