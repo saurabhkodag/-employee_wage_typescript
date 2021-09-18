b3115eab0c1bd7e331a5a01a330385891579678e
Sourabh Madhukar Kodag <kodag.saurabh6@gmail.com>
2021-09-19 05:09:53 +0530

    [sourabh] added UC8_Wage_Array

--- demo.ts
+++ demo.ts
@@ -226,3 +226,39 @@
 //     break;
 
 // }
+
+
+// UC8
+  
+// const con= require('./constant')
+// let wage:number[]=[];
+// function hrs(key){
+//   switch(key){
+//       case 0:
+//       emphrs=con.fulltime;
+//       break;
+//     case 1:
+//       emphrs=con.parttime;
+//       break;
+//     default:
+//       emphrs=0;
+//   }
+//   return emphrs;
+//   }
+// var emphrs:number=0;
+// var empwage:number=0;
+// var totalhrs:number=0;
+// var i:number=0;
+// while(i<=20){
+// let key:number =Math.floor(Math.random()*10%3)
+//   emphrs=hrs(key);
+//   totalhrs+=emphrs;
+//   empwage+=emphrs*con.wageperhour;
+// wage.push(empwage);
+// if(totalhrs>=160)
+// break;
+// i++;
+// }
+// i--;
+// console.log("DAY",i,"EmpHr:",totalhrs, "EmpWage:",empwage);
+// console.log(wage);
